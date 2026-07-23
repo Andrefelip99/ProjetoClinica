433b8f6c0f25b2b4e1dc3c982878d04b8a14da8c
andre felipe <andre.felipe106@icloud.com>
2026-07-23 14:01:10 -0300

    Test

--- src/main/java/projeto_clinica/com/config/CorsConfig.java
+++ src/main/java/projeto_clinica/com/config/CorsConfig.java
@@ -10,7 +10,11 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://frontend-saude-andrefelipedasilva0599-3007s-projects.vercel.app")
+                .allowedOrigins(
+                    "http://localhost:5500",
+                    "http://127.0.0.1:5500",
+                    "https://frontend-saude-andrefelipedasilva0599-3007s-projects.vercel.app"
+                )
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(false);
